574a80e222603c2d252ab3e5c83c680383f31eee
Isidro Cuate <jrvelez724@gmail.com>
2021-02-13 00:39:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,12 +4,20 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+def crew
+["Hello Junior.", "Hello Sebastian.", "Hello Iselly."]
+end
+
+def greeting_crew(crew) 
+  crew.each {|crew_member| "Hello#{crew member}."}
+end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  state_log(date)
+  greeting_crew(crew)
 end

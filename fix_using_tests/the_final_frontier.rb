19443e46908a99a8193df5442b172278d59560f3
Isidro Cuate <jrvelez724@gmail.com>
2021-02-13 00:48:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,6 +3,7 @@
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
+
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
@@ -18,6 +19,6 @@ end
 
 def engage
   date = generate_star_date
-  state_log(date)
-  greeting_crew(crew)
+  state_log
+  greeting_crew
 end
